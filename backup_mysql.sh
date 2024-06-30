06afd65e4ee18efb1dc1bac48bf9fc3b99453e14
lalithbarani <130025078+lalithbarani@users.noreply.github.com>
2024-06-30 21:24:02 +0530

    Update and rename db_backup.sh to backup_mysql.sh

--- backup_mysql.sh
+++ backup_mysql.sh
@@ -2,10 +2,8 @@
 host="localhost"
 pass=""
 user="root"
-
-db_name="urban_green"
+db_name="your database name "
 
 backup="./backups-$(date +%F).sql"
 mysqldump -u root --password="$pass"  urban_green > $backup
 find ./ -type f -name 'backups-*.sql' -mtime +7 -exec rm {} \;
-find ./ -type f -name 'backups-*.sql'  -mtime +7 -exec rm {} \; 
